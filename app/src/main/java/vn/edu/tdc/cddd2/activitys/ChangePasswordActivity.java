75ece4db0e856b6b7da8499307854f459c3e1db8
TenLaDanh <tenladanh12@gmail.com>
2021-10-16 22:11:57 +0700

    update dialog

--- app/src/main/java/vn/edu/tdc/cddd2/activitys/ChangePasswordActivity.java
+++ app/src/main/java/vn/edu/tdc/cddd2/activitys/ChangePasswordActivity.java
@@ -1,12 +1,20 @@
 package vn.edu.tdc.cddd2.activitys;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,24 +27,34 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import vn.edu.tdc.cddd2.DAO.DAOAccount;
 import vn.edu.tdc.cddd2.R;
+import vn.edu.tdc.cddd2.data_models.Account;
 
 public class ChangePasswordActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     // Khai báo biến:
+    private String username = "admin";
     private Button btnChange;
     private Intent intent;
     private Toolbar toolbar;
     private TextView btnBack, subtitleAppbar;
+    private EditText edtOldPassword,edtNewPassword,edtConfirmPassword;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
-
+    private DatabaseReference ref = FirebaseDatabase.getInstance("https://cddd2-f1bcd-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Account");
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_changepassword);
 
+       // username = getIntent().getExtras().getString("username");
         // Toolbar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -54,12 +72,38 @@ public class ChangePasswordActivity extends AppCompatActivity implements Navigat
         // Khởi tạo biến:
         btnChange = findViewById(R.id.btnLogin);
         btnBack = findViewById(R.id.txtBack);
-
+        edtOldPassword = findViewById(R.id.edtPass);
+        edtNewPassword = findViewById(R.id.edtPassNew);
+        edtConfirmPassword = findViewById(R.id.edtPassConfirm);
+        openDialog(Gravity.CENTER);
         // Sự kiện xử lý button "Đổi mật khẩu"
         btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                DAOAccount dao = new DAOAccount();
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot node : snapshot.getChildren()){
+                            Account account = node.getValue(Account.class);
+                            if(account.getUsername().compareTo(username) == 0 && account.getPassword().compareTo(String.valueOf(edtOldPassword.getText())) == 0){
+                                if(String.valueOf(edtNewPassword.getText()).compareTo(String.valueOf(edtConfirmPassword.getText())) == 0){
+                                    account.setPassword(String.valueOf(edtNewPassword.getText()));
+                                    dao.update(node.getKey(),account).addOnSuccessListener(suc -> {
+                                        openDialog(Gravity.CENTER);
+                                    }).addOnFailureListener(err ->{
+
+                                    });
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
 
@@ -72,7 +116,32 @@ public class ChangePasswordActivity extends AppCompatActivity implements Navigat
         });
 
     }
+    public void openDialog(int gravity){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_success);
+        Window window = dialog.getWindow();
+        if(window == null) return;
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        WindowManager.LayoutParams windowAttribute = window.getAttributes();
+        windowAttribute.gravity = gravity;
+        window.setAttributes(windowAttribute);
+        dialog.setCancelable(false);
+        TextView txtContent = dialog.findViewById(R.id.txtNotifyContent);
+        Button btnOK = dialog.findViewById(R.id.btnOK);
+        txtContent.setText("Đổi mật khẩu thành công !");
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent1 = new Intent(ChangePasswordActivity.this, MainADMActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent1);
 
+            }
+        });
+        dialog.show();
+    }
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
